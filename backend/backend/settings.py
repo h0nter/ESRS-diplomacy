7c38b64dd4486e5180da1385dc0773779ae6b33d
Tym Makowski <tymoteuszmakowski@gmail.com>
2023-02-11 22:11:58 +0000

    Added master and room app to the settings
    Master and room app were missing from the list of installed apps so I added them.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'master.apps.MasterConfig',
+    'room.apps.RoomConfig',
 ]
 
 MIDDLEWARE = [
