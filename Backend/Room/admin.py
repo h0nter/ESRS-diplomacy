ee4e965138d68d1c00ceb72998428ecfffe28d3e
Aniels <96707275+Aniels@users.noreply.github.com>
2023-02-13 17:19:25 +0000

    graphQL_demo
    created a GraphQL API for demonstration and also solved the merging problem

--- Backend/Room/admin.py
+++ Backend/Room/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models.graph_demo import Category, Book, Grocery
 
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Book)
+admin.site.register(Grocery)
