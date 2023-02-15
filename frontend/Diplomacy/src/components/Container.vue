4e0a21aab8eb36132f96ddf4e634b4bae589a30a
Tym Makowski <tymoteuszmakowski@gmail.com>
2023-02-15 13:58:44 +0000

    Home and Game view
    Cleaned up the code of unused template components and implemented simple routing for 2 views (Home, Game).

--- frontend/Diplomacy/src/components/Container.vue
+++ frontend/Diplomacy/src/components/Container.vue
@@ -1,9 +1,15 @@
 <template>
-  <div class="container lg:max-w-screen-lg mx-auto px-5 py-5">
+  <div class="container lg:max-w-screen-lg px-5 py-5">
       <slot />
     </div>
 </template>
 
 <style scoped>
-
+  .container{
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
 </style>
--- frontend/Diplomacy/src/router/index.ts
+++ frontend/Diplomacy/src/router/index.ts
@@ -1,23 +1,36 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import HomeView from "@/views/HomeView.vue";
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
+const routes = [
     {
       path: "/",
       name: "home",
       component: HomeView,
+      alias: "/home"
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
+      path: "/game",
+      name: "game",
+      component: () => import("@/views/GameView.vue"),
     },
-  ],
+    // {
+    //   path: '*',
+    //   redirect: '/'
+    // }
+
+    // {
+    //   path: "/about",
+    //   name: "about",
+    //   // route level code-splitting
+    //   // this generates a separate chunk (About.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import("../views/AboutView.vue"),
+    // },
+];
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes
 });
 
 export default router;
