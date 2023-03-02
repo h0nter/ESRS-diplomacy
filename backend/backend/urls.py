4f01de551ae5f3494f82afb5e55220933fce9a24
Joe C <52496811+Jcorb08@users.noreply.github.com>
2023-03-02 11:24:06 +0000

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.decorators.csrf import csrf_exempt
+#from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
 from graphqlAPI.schema import schema
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
+    path("graphql", GraphQLView.as_view(graphiql=True, schema=schema)),
     path("", include('room.urls'))
 ]
