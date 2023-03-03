f7e736107d885a5ae20151b7a4656cfa588a7ed2
Aniels <96707275+Aniels@users.noreply.github.com>
2023-03-03 08:18:51 +0000

    reset satabase

--- backend/graphqlAPI/tests.py
+++ backend/graphqlAPI/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+import json
+from graphene_django.utils.testing import GraphQLTestCase
 
-# Create your tests here.
+class MyFancyTestCase(GraphQLTestCase):
+    def test_some_query(self):
+        response = self.query(
+            '''
+            query{
+                orders{
+                    id
+                    targetUnit{
+                        id
+                    }
+                }
+            }
+            ''',
+            op_name='myModel'
+        )
+
+        content = json.loads(response.content)
+
+        # This validates the status code and if you get errors
+        self.assertResponseNoErrors(response)
+
+        # Add some more asserts if you like
+        ...
