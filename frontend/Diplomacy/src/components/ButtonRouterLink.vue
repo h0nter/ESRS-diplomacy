32a6695bab62ee13fbc32a7c6ab8da93f3042e74
Tym Makowski <tymoteuszmakowski@gmail.com>
2023-03-29 01:16:09 +0100

    Container component test

--- frontend/Diplomacy/src/components/ButtonRouterLink.vue
+++ frontend/Diplomacy/src/components/ButtonRouterLink.vue
@@ -5,6 +5,8 @@
 </template>
 
 <script setup>
+import { RouterLink } from "vue-router";
+
 const props = defineProps({
   url: String,
   text: String
--- frontend/Diplomacy/src/components/__tests__/Container.spec.ts
+++ frontend/Diplomacy/src/components/__tests__/Container.spec.ts
@@ -1,14 +1,14 @@
-import {mount, shallowMount} from "@vue/test-utils";
+import {shallowMount} from "@vue/test-utils";
 import { describe, it, expect } from "vitest";
 import Container from "../Container.vue";
 
 describe("Container", () => {
     it ("renders properly", () => {
-        const wrapper = mount(Container,{
+        const wrapper = shallowMount(Container,{
             slots: {
                 default: '<h1>Hello Vitest</h1>'
             }
         });
-        expect(wrapper.html()).toContain("Hello Vitest");
+        expect(wrapper.html()).toContain('<h1>Hello Vitest</h1>');
     });
 });
