afaf655d45c4baadf1541e722e465f6f5a849af9
Jcorb08 <jcorb8800@gmail.com>
2023-04-07 10:44:17 +0100

    support tests failing currently

--- backend/room/tests/test.py
+++ backend/room/tests/test.py
@@ -16,17 +16,25 @@ class room_app_unitTest1(TestCase):
         cls.locationA = Location.objects.create(name="location A",map=cls.map)
         cls.locationB = Location.objects.create(name="location B",map=cls.map)
         cls.locationC = Location.objects.create(name='location C',map=cls.map)
+        cls.locationD = Location.objects.create(name='location D',map=cls.map)
         cls.nextToAB = Next_to.objects.create(location=cls.locationA, next_to=cls.locationB)
         cls.nextToBA = Next_to.objects.create(location=cls.locationB, next_to=cls.locationA)
         cls.nextToCB = Next_to.objects.create(location=cls.locationC, next_to=cls.locationB)
         cls.nextToBC = Next_to.objects.create(location=cls.locationB, next_to=cls.locationC)
+        cls.nextToAD = Next_to.objects.create(location=cls.locationA, next_to=cls.locationD)
+        cls.nextToDA = Next_to.objects.create(location=cls.locationD, next_to=cls.locationA)
         cls.unitA = Unit.objects.create(owner=cls.countryA,location=cls.locationA)
+        cls.unitB = Unit.objects.create(owner=cls.countryA,location=cls.locationC)
         cls.turn = Turn.objects.create(year=1994)
-        cls.orderA = Order.objects.create(instruction='MVE', turn=cls.turn, target_unit=cls.unitA, current_location=cls.locationA, target_location=cls.locationB)
-        cls.orderB = Order.objects.create(instrucion='MVE', turn=cls.turn, target_unit=cls.unitA, current_location=cls.locationA, target_location=cls.locationC)
+        cls.orderMVEValid = Order.objects.create(instruction='MVE', turn=cls.turn, target_unit=cls.unitA, current_location=cls.locationA, target_location=cls.locationB)
+        cls.orderMVEInvalid = Order.objects.create(instruction='MVE', turn=cls.turn, target_unit=cls.unitA, current_location=cls.locationA, target_location=cls.locationC)
+        cls.orderSPTValid = Order.objects.create(instruction='SPT', turn=cls.turn, target_unit=cls.unitB, current_location=cls.locationC, reference_unit=cls.unitA,\
+                                                  reference_unit_current_location=cls.locationA, reference_unit_new_location=cls.locationB)
+        cls.orderSPTInvalid = Order.objects.create(instruction='SPT', turn=cls.turn, target_unit=cls.unitB, current_location=cls.locationC, reference_unit=cls.unitA,\
+                                                  reference_unit_current_location=cls.locationA, reference_unit_new_location=cls.locationD)
 
     def test_build(self):
-        army = self.orderA.target_unit
+        army = self.orderMVEValid.target_unit
         self.assertTrue(type(army) is Unit)
         self.assertTrue(army == self.unitA)
         self.assertFalse(army.can_float)
@@ -39,23 +47,38 @@ class room_app_unitTest1(TestCase):
     #Each test takes self as a parameter
     def test_before_move(self):
         #self then relates to the database objects
-        army = self.orderA.target_unit
-        self.assertNotEqual(army.location,self.orderA.target_location)
+        army = self.orderMVEValid.target_unit
+        self.assertNotEqual(army.location,self.orderMVEValid.target_location)
 
 
     #def test_abs(self):
         #abs = AbstractUnit(self.orderA)
         #self.assertEqual(abs.order, self.orderA)
 
-    # def test_valid_move(self):
+    def test_valid_move(self):
+        unit:Unit = self.orderMVEValid.target_unit
+        self.assertEqual(self.orderMVEValid.instruction,'MVE')
+        self.assertTrue(unit.validate_move(self.orderMVEValid))
         
 
     def test_move(self):
-        army = self.orderA.target_unit
+        army = self.orderMVEValid.target_unit
         before_location = army.location 
-        army.move(self.orderA.target_location)
+        army.move(self.orderMVEValid.target_location)
         self.assertNotEqual(before_location,army.location)
         self.assertNotEqual(self.locationA,self.unitA.location)
 
-    # def test_invalid_move(self):
-    #     s
+    def test_invalid_move(self):
+        unit:Unit = self.orderMVEInvalid.target_unit
+        self.assertEqual(self.orderMVEInvalid.instruction,'MVE')
+        self.assertFalse(unit.validate_move(self.orderMVEInvalid))
+
+    def test_valid_support(self):
+        unit:Unit = self.orderSPTValid.target_unit
+        self.assertEqual(self.orderSPTValid.instruction,'SPT')
+        self.assertTrue(unit.validate_support(self.orderSPTValid,self.turn))
+
+    def test_invalid_support(self):
+        unit:Unit = self.orderSPTInvalid.target_unit
+        self.assertEqual(self.orderSPTInvalid.instruction,'SPT')
+        self.assertFalse(unit.validate_support(self.orderSPTInvalid,self.turn))
