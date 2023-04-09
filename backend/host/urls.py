69622e295ad776a996f27f306d988755ac25848c
Aniels <96707275+Aniels@users.noreply.github.com>
2023-04-09 15:50:55 +0100

    recover host model branch

--- backend/host/urls.py
+++ backend/host/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-# from . import views
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import LoginView
+from . import views
 
 
-urlpatterns = [
-#     path('', views.index, name='index'),
-#     path('register/', views.register, name='register')
+
+urlpatterns =[
+    path('', views.index, name='index'),
+    path('register/', views.registration, name='register'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('login/', LoginView.as_view(template_name='host/login.html'), name='login'),
 ]
-
--- backend/host/views.py
+++ backend/host/views.py
@@ -1,26 +1,47 @@
-# from django.http import HttpResponse
-# from django.http import HttpResponseNotAllowed
-# from django.views.decorators.csrf import csrf_exempt
-# import secrets
-# from .models import User
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from .forms import CustomUserCreationForm
+from .models import Room
+from room.models.order import Turn
+
+# Create your views here.
+def index(request):
+    return HttpResponse('index page')
 
 
-# # Create your views here.
-# def index(request):
-#     return HttpResponse('index page')
+def registration(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # Authenticate and log in the user
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(request, username=username, password=password)
+            login(request, user)
+            return redirect('/')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'host/register.html', {'form': form})
 
-# @csrf_exempt
-# def register(request):
-#     if request.method == 'POST':
-#         name = request.POST.get("name")
-#         try:
-#             user = User.objects.get(name=name)
-#             return HttpResponse('user already exist')
-#         except User.DoesNotExist:
-#             token = secrets.token_hex(16)
-#             user = User(name=name, cookies = token)
-#             user.save()
-#             return HttpResponse('here is your cookies').set_cookie('cookie_name', token)
-#     else:
-#         return  HttpResponseNotAllowed(['POST'])
-    
+# useing django build-in function to authenticate user
+def something_else(request):
+    if request.user.is_authenticated:
+        # Do something for authenticated users.
+        ...
+    else:
+        # Do something for anonymous users.
+        ...
+
+
+
+def launch_room(request):
+    turn = Turn.objects.create(year=1901)
+    user = request.user
+    room = Room.objects.create(turn=turn, hoster=user)
+    room.save()
+    room.player.add(user)
+   
+
+
