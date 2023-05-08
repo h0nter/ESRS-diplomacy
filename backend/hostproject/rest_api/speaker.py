a43c248fe99eb15014bae22ce592559d85abc954
Aniels <96707275+Aniels@users.noreply.github.com>
2023-05-08 21:10:10 +0100

    issue with room name fixed

--- backend/hostproject/rest_api/speaker.py
+++ backend/hostproject/rest_api/speaker.py
@@ -1,7 +1,7 @@
 import requests
 
 
-def game_trigger(port, room_name:str):
+def add_room(port, room_name:str):
     url = "http://127.0.0.1:"+str(port)+"/graphql/"
     query_template = '''
     mutation{
@@ -34,6 +34,25 @@ def add_player(port:int, user_id:int, room_id:int):
         }
     '''
     query = query_template % (user_id, room_id)
+    headers = { 'Content-Type': 'application/json' }
+    data = {'query': query}
+    res = requests.post(url, headers=headers, json=data).json()
+    print(res)
+    
+    return res
+
+def initialize(port:int, room_id:int):
+    url = "http://127.0.0.1:"+str(port)+"/graphql/"
+    query_template = '''
+        mutation{
+            initilizeRoom(roomId: %s){
+                player{
+                    id
+                }
+            }
+        }
+    '''
+    query = query_template % room_id
     headers = { 'Content-Type': 'application/json' }
     data = {'query': query}
     res = requests.post(url, headers=headers, json=data).json()
--- backend/hostproject/rest_api/views.py
+++ backend/hostproject/rest_api/views.py
@@ -2,25 +2,24 @@ from django.contrib.auth import login, authenticate
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from host.models import Host, RoomStatus, UserRoom
-from .speaker import add_player, game_trigger
+from .speaker import add_player, add_room
 
 @api_view()
 def hello_world(request):
     return Response({"message": "Hello, world!"})
 
 
-
 @api_view(["POST"])
 def start_game(request):
     if request.method == 'POST':
         room = Host.objects.get(pk=request.POST.get("room_id"))
-        room.room_name = RoomStatus.WAITING
-        response_data = game_trigger(port=room.port, room_name=room.room_name)
+        room.status = RoomStatus.INITIALIZE
+        response_data = add_room(port=room.port, room_name=room.room_name)
         room.room_id = response_data['data']['createRoom']['room']['id']
         room.save()
         
         for user in UserRoom.objects.filter(room=room):
-            add_player(port=room.port, user_id=user.pk, room_id=room.pk)
+            add_player(port=room.port, user_id=user.user.pk, room_id=room.pk)
         
         return Response(response_data)
 
